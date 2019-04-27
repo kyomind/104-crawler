3dbd1b97895eae169465c12d445b3fb967f04976
kyo <odinxp@gmail.com>
2019-04-28 02:36:01 +0800

    add a lot

---
+++ test.py
@@ -0,0 +1,5 @@
+https://www.104.com.tw/jobs/search/?ro=0&keyword=php&area=6001001000%2C6001002000&order=1&asc=0&page=1&mode=s&jobsource=2018indexpoc
+https://www.104.com.tw/jobs/search/?ro=0&keyword=php&area=6001001000%2C6001002000&order=1&asc=0&page=15&mode=s&jobsource=2018indexpoc
+https://www.104.com.tw/jobs/search/?ro=0&keyword=php&area=6001001000%2C6001002000&order=1&asc=0&page=27&mode=s&jobsource=2018indexpoc
+https://www.104.com.tw/jobs/search/?ro=0&kwop=7&keyword=php&area=6001001000%2C6001002000&order=1&asc=0&page=16&mode=s&jobsource=2018indexpoc
+處理中，第 644 項	處理中，第 645 項	處理中，第 646 項	處理中，第 647 項	處理中，第 648 項
